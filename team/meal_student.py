789436f80b46b2d0a563283f251781700e53b6af
ando00 <148081353+ando00@users.noreply.github.com>
2023-11-28 17:30:37 +0900

    Update meal_student.py

--- team/meal_student.py
+++ team/meal_student.py
@@ -15,35 +15,31 @@ def scrape_and_write_to_csv(url, meal_type, csv_file, target_date):
     soup = BeautifulSoup(response.text, 'html.parser')
     
     menu_elements = soup.find_all('td', class_='px-4 py-3 text-center')
-    menu_list = [menu.text for menu in menu_elements]
+    menu_values = [element.text for element in menu_elements]
 
-    calorie_element = soup.find('td', class_='px-4 py-3 text-center', string='총 칼로리:')
-    calorie = calorie_element.text if calorie_element else None
-    
+    # Extract only the first value from each element
+    menu_list = [menu_values[i] for i in range(0, len(menu_values), 3)]
+
     with open(csv_file, mode='a', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
-        
+
         # Data
-        for i in range(0, len(menu_list), 3):
-            writer.writerow([menu_list[i], menu_list[i + 1], menu_list[i + 2], meal_type, target_date])
-        
-        # Total Calorie
-        writer.writerow(["총 칼로리", "", calorie, meal_type, target_date])
+        writer.writerow([" ".join(menu_list), meal_type, target_date])
 
 # Define meal types
-meal_types = ["breakfast", "lunch", "dinner"]
+meal_types = ["breakfast","lunch","dinner"]
 
 # Define CSV file path
-csv_file_path = "menu_fff.csv"
+csv_file_path = "menu_student.csv"
 
 # Write header to CSV file
 with open(csv_file_path, mode='w', encoding='utf-8', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["메뉴", "타입", "칼로리", "시간"])
+    writer.writerow(["메뉴", "타입", "시간"])
 
-# Loop through next 7 days
+# Loop through next 5 days or until Saturday
 current_date = datetime.now()
-for _ in range(7):
+for _ in range(5):
     target_date = current_date.strftime("%Y-%m-%d")
     
     # Loop through meal types and scrape, and write to CSV
@@ -51,9 +47,11 @@ for _ in range(7):
         meal_url = get_meal_url_for_date(meal_type, target_date)
         scrape_and_write_to_csv(meal_url, meal_type, csv_file_path, target_date)
     
+    # Check if current day is Saturday, and exit the loop if true
+    if current_date.weekday() == 4:  # 5 corresponds to Saturday
+        break
+    
     current_date += timedelta(days=1)
 
-print("7일간의 파일이 생성되었습니다.")
-
-
+print("5일간의 파일이 생성되었습니다.")
     
