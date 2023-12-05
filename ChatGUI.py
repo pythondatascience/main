27bab852644ed01d06d9bb15a232b612a11571f5
happyhug <89500437+happyhug@users.noreply.github.com>
2023-12-06 02:59:56 +0900

    Update ChatGUI.py
    - Changed the ChatBot Name and Window Title
    - Changed the conditions to exit the window.

--- ChatGUI.py
+++ ChatGUI.py
@@ -26,19 +26,23 @@ tomorrow = today + datetime.timedelta(days=1)
 def chatPNU(event = None):
     user_input = user_input_field.get()
     
-    if user_input.lower() == "종료" or user_input.lower() == "종료 ":
+    # if user_input.lower() == "종료" or user_input.lower() == "종료 ":
+    #     PNU_window.destroy()  # close window
+    #     return
+#################################################################################### 해당부분 수정
+    if '종료' in user_input :
         PNU_window.destroy()  # close window
         return
-
+####################################################################################
     chat_field.insert(tkinter.END, "나: " + user_input + "\n", "User_color")
-    chat_field.insert(tkinter.END, "ChatPNU: " + answer(user_input) + "\n")
+    chat_field.insert(tkinter.END, "PNU챗봇: " + answer(user_input) + "\n")
     user_input_field.delete(0, tkinter.END)
 
     chat_field.see(tkinter.END)
 
 # Setting GUI windows
 PNU_window = tkinter.Tk()
-PNU_window.title("ChatPNU")
+PNU_window.title("부산대학교전용 PNU챗봇")
 
 customFont = font.Font(size=20)
 
@@ -48,7 +52,7 @@ scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 chat_field = tkinter.Text(PNU_window, height=15, width=50, font=customFont, yscrollcommand=scrollbar.set)
 chat_field.pack()
 
-chat_field.insert(tkinter.END, "ChatPNU: " + answer("자기소개 해 줘!") + "\n")
+chat_field.insert(tkinter.END, "PNU챗봇: " + answer("자기소개 해 줘!") + "\n")
 
 notice = "* (식단정보) 오늘(" + f"{today}" + ")과 내일(" + f"{tomorrow}" + "일)이 아닌 다른날의 정보는 다를 수 있습니다."
 message=tkinter.Message(PNU_window, text=notice, width=700)
