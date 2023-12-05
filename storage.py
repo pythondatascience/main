0f30837f7854bde3c5f3b7c57f858151ffb1f425
ando00 <148081353+ando00@users.noreply.github.com>
2023-12-05 20:44:54 +0900

    Add files via upload
    storage 수정했습니다

--- storage.py
+++ storage.py
@@ -1,12 +1,12 @@
 import os
 import subprocess
 from datetime import datetime, timedelta
-
+import Prompting
 # 스크립트와 CSV 파일 이름의 쌍을 정의합니다.
 script_csv_pairs = {
-    "menu_student.py": "menu_student.csv",
-    "공지사항.py": "Notice.csv",
-    "학사일정.py": "schedule_data.csv"
+    "menu.py": "menu.csv",
+    "notice.py": "notice.csv",
+    "schedule.py": "schedule.csv"
     }
 
 # 현재 작업 디렉토리를 변경합니다. (파이썬 파일이 존재하는 경로)
@@ -16,19 +16,23 @@ os.chdir("/home/pnuee/menu")
 for script, csv_file in script_csv_pairs.items():
     # CSV 파일이 이미 존재하는지 확인합니다. == 처음 실행 되었을 때
     if os.path.isfile(csv_file):
-        # CSV 파일의 생성 날짜를 가져옵니다.
-        csv_creation_date = datetime.fromtimestamp(os.path.getctime(csv_file)).date()
+        # CSV 파일의 수정 시간을 가져옵니다.
+        # csv_creation_date = datetime.fromtimestamp(os.path.getctime(csv_file)).date()
+        csv_modification_time = datetime.fromtimestamp(os.path.getmtime(csv_file)).date()
+        # print(csv_creation_date)
+        # print(csv_modification_time)
         
         # 현재 날짜를 가져옵니다.
         current_date = datetime.now().date()
 
-        # CSV 파일이 생성된 날짜와 현재 날짜가 다르면 해당 스크립트를 다시 실행합니다.
-        if csv_creation_date != current_date:
+        # CSV 파일이 수정된 날짜와 현재 날짜가 다르면 해당 스크립트를 다시 실행합니다.
+        # if csv_creation_date != current_date:
+        if csv_modification_time != current_date:
             command = f"python {script}"
             subprocess.run(command, shell=True)
             print(f"{csv_file}이(가) 업데이트되었습니다.")
         else:
-            print(f"{csv_file} 파일이 이미 존재합니다.") # 같은 날이면 기존 csv파일 사용
+            print(f"{csv_file} 파일이 이미 존재합니다.")  # 같은 날이면 기존 csv파일 사용
     else:
         # CSV 파일이 없으면 해당 스크립트를 실행합니다.
         command = f"python {script}"
@@ -36,3 +40,11 @@ for script, csv_file in script_csv_pairs.items():
         print(f"{csv_file}이(가) 생성되었습니다.")
 
 print("모든 스크립트가 실행되었습니다.")
+
+menu_csv_filename = 'menu.csv'
+notice_csv_filename = 'notice.csv'
+schedule_csv_filename = 'schedule.csv'
+txt_filename = 'PDSPrompt.txt'
+new_column_order = ['날짜', '타입', '식사시간', '메뉴']
+
+Prompting.csv_to_txt(menu_csv_filename, notice_csv_filename, schedule_csv_filename, txt_filename, new_column_order)
